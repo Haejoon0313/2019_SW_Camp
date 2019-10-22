710adcdd016f2d1e7cf0196840c6076f0c488ff7
Haejoon0313 <sam960313@gmail.com>
2019-10-22 15:44:58 +0900

    arduino capacitive sensor lib add

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,19 @@
+#include <CapacitiveSensor.h>
+
+CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);
+
 void setup() {
   // put your setup code here, to run once:
+  cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Hello World");
+  long start = millis();
+  long total1 =  cs_4_2.capacitiveSensor(30);
+  
+  Serial.println(total1);                  // print sensor output 1
+  
   delay(1000);
 }
-
-
