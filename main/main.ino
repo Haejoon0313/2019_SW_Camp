d3dffd3cfe6d1f28729848d70fd7a7886da72912
Haejoon0313 <sam960313@gmail.com>
2019-10-22 22:39:54 +0900

    new plan

--- main/main.ino
+++ main/main.ino
@@ -1,7 +1,7 @@
 #define SOUND_SENSOR_PIN A0
 #define RED_PIN 2
-#define YELLOW_PIN 4
-#define GREEN_PIN 7
+#define YELLOW_PIN 3
+#define GREEN_PIN 4
 
 int sensorValue;
 
@@ -18,9 +18,9 @@ void loop() {
   sensorValue = analogRead(SOUND_SENSOR_PIN);
   Serial.println(sensorValue);
   if(sensorValue < 570){
-    digitalWrite(RED_PIN, HIGH);
+    digitalWrite(RED_PIN, LOW);
     digitalWrite(YELLOW_PIN, LOW);
-    digitalWrite(GREEN_PIN, LOW);
+    digitalWrite(GREEN_PIN, HIGH);
   }      
   else if(sensorValue < 590){
     digitalWrite(RED_PIN, LOW);
@@ -28,9 +28,9 @@ void loop() {
     digitalWrite(GREEN_PIN, LOW);
   }
   else if(sensorValue < 620){  
-    digitalWrite(RED_PIN, LOW);
+    digitalWrite(RED_PIN, HIGH);
     digitalWrite(YELLOW_PIN, LOW);
-    digitalWrite(GREEN_PIN, HIGH);
+    digitalWrite(GREEN_PIN, LOW);
   }
   else{
     digitalWrite(RED_PIN, HIGH);
