3a7566ccb3c7d146061c9e5911e501dffe5d9d96
Haejoon0313 <sam960313@gmail.com>
2019-10-22 22:09:01 +0900

    sound sensor sample & led pin

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,41 @@
+#define SOUND_SENSOR_PIN A0
+#define RED_PIN 2
+#define YELLOW_PIN 4
+#define GREEN_PIN 7
+
+int sensorValue;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(YELLOW_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  sensorValue = analogRead(SOUND_SENSOR_PIN);
+  Serial.println(sensorValue);
+  if(sensorValue < 570){
+    digitalWrite(RED_PIN, HIGH);
+    digitalWrite(YELLOW_PIN, LOW);
+    digitalWrite(GREEN_PIN, LOW);
+  }      
+  else if(sensorValue < 590){
+    digitalWrite(RED_PIN, LOW);
+    digitalWrite(YELLOW_PIN, HIGH);
+    digitalWrite(GREEN_PIN, LOW);
+  }
+  else if(sensorValue < 620){  
+    digitalWrite(RED_PIN, LOW);
+    digitalWrite(YELLOW_PIN, LOW);
+    digitalWrite(GREEN_PIN, HIGH);
+  }
+  else{
+    digitalWrite(RED_PIN, HIGH);
+    digitalWrite(YELLOW_PIN, HIGH);
+    digitalWrite(GREEN_PIN, HIGH);
+  }
+  delay(100);
 }
