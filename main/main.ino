2596dc027f9cd2981cb1d5426fb096f1525297aa
Haejoon0313 <sam960313@gmail.com>
2019-10-23 15:17:50 +0900

    RGB Pin setting & function declare

--- main/main.ino
+++ main/main.ino
@@ -1,15 +1,15 @@
 #define SOUND_SENSOR_PIN A0
-#define RED_PIN 2
-#define YELLOW_PIN 3
-#define GREEN_PIN 4
+#define RED_PIN 3
+#define GREEN_PIN 5
+#define BLUE_PIN 6
 
 int sensorValue;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(RED_PIN, OUTPUT);
-  pinMode(YELLOW_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -17,25 +17,26 @@ void loop() {
   // put your main code here, to run repeatedly:
   sensorValue = analogRead(SOUND_SENSOR_PIN);
   Serial.println(sensorValue);
-  if(sensorValue < 570){
-    digitalWrite(RED_PIN, LOW);
-    digitalWrite(YELLOW_PIN, LOW);
-    digitalWrite(GREEN_PIN, HIGH);
-  }      
-  else if(sensorValue < 590){
-    digitalWrite(RED_PIN, LOW);
-    digitalWrite(YELLOW_PIN, HIGH);
-    digitalWrite(GREEN_PIN, LOW);
-  }
-  else if(sensorValue < 620){  
-    digitalWrite(RED_PIN, HIGH);
-    digitalWrite(YELLOW_PIN, LOW);
-    digitalWrite(GREEN_PIN, LOW);
-  }
-  else{
-    digitalWrite(RED_PIN, HIGH);
-    digitalWrite(YELLOW_PIN, HIGH);
-    digitalWrite(GREEN_PIN, HIGH);
-  }
+  
+  ledPrint(sensorValue);
+  
   delay(100);
+}
+
+void ledPrint(int sensorValue){
+  if(sensorValue < 580){
+    analogWrite(RED_PIN, 0);
+    analogWrite(GREEN_PIN, 50);
+    analogWrite(BLUE_PIN, 0);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+  }      
+  else if(sensorValue < 600){
+    analogWrite(RED_PIN, 40);
+    analogWrite(GREEN_PIN, 20);
+    analogWrite(BLUE_PIN, 0);
+  }
+  else{  
+    analogWrite(RED_PIN, 50);
+    analogWrite(GREEN_PIN, 0);
+    analogWrite(BLUE_PIN, 0); 
+  }
 }
